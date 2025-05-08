4219e84a2571d46820b7d3638c4c91b98e8837af
Marko Punčec <mpuncec46@gmail.com>
2025-05-08 21:01:19 +0200

    Main model save point
    MP

---
+++ ToDoAPI/Models/ToDoListItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoAPI.Models
+{
+    public class ToDoListItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string TextContent { get; set; }
+        public bool Completed { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string Priority { get; set; }
+    }
+}
